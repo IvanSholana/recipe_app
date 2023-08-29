ee832f385bf746e3d9b4b8b68c1afeb762197248
Ivan Sholana <ivansholana@gmail.com>
2023-08-29 21:06:24 +0700

    add app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,13 +15,41 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
+        elevation: 4,
+        shadowColor: Colors.black45,
+        toolbarHeight: 120,
         title: Column(
           children: [
             TextField(
-                decoration: InputDecoration(
-              filled: true,
-            )),
+              decoration: InputDecoration(
+                filled: true,
+                hintText: "Cari Kategori...",
+                suffixIcon: Icon(Icons.search),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Hello World"),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Hello World"),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Hello World"),
+                )
+              ],
+            )
           ],
         ),
       ),
